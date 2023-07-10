ec4e4be709e710603ba62350a9376efe74e13628
ezequielmalfonso <ezequielmalfonso@gmail.com>
2023-07-10 00:54:49 +0000

    logs

--- memoria/src/memoriaComunicacion.c
+++ memoria/src/memoriaComunicacion.c
@@ -287,11 +287,11 @@ static void procesar_fileSystem(void * void_args) {
         		  recv_fs_memoria_read(cliente_socket, direccion_fisica, &tamanio, leido);
         		  char** seg_desp_pid =  string_split(direccion_fisica, "x");
 
-        		  //log_info(logger, "PID: %d - N° Segmento: %d, Desplazamiento: %d, Tamanio: %d", *pid, *num_seg, *desplazamiento1, *tamanio);
+        		  log_info(logger, "PID: %d - N° Segmento: %d, Desplazamiento: %d, Tamanio: %d", *pid, *num_seg, *desplazamiento1, *tamanio);
 				  //log_info(logger, "xPID: %lu - N° Segmento: %lu, Desplazamiento: %lu, Tamanio: %d", *pid, *num_seg, *desplazamiento1, *tamanio);
-				  leido = leerMemoria(*num_seg, *desplazamiento1, *pid, tamanio);
+				  leido = leerMemoria(*num_seg, *desplazamiento1, *pid, *tamanio);
         		  log_error(logger, "Explote de la dir fisica: Seg: %s - Desp: %s - Pid: %s", seg_desp_pid[0], seg_desp_pid[1], seg_desp_pid[2]);
-        		  log_warning(logger, "Recibimos dir fisica: %s - Tamanio: %d",direccion_fisica, tamanio);
+        		  log_warning(logger, "Recibimos dir fisica: %s - Tamanio: %d",direccion_fisica, *tamanio);
 
         		  //TODO
         		  op_code cop = MOV_IN_OK;
